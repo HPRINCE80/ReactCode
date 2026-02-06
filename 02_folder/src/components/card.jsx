7e3a851a654d2f47da23331cc825a5f91e9d17ed
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-06 19:57:20 +0530

    Day 2

---
+++ 02_folder/src/components/card.jsx
@@ -0,0 +1,8 @@
+function Card() {
+    return   <div className="card">
+        <h1>Prince Yaduvasnhi</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia totam temporibus incidunt dolorum assumenda excepturi rerum accusamus, 
+        ad aliquid quaerat quam minima atque magnam obcaecati ipsum labore eligendi laudantium odio!
+      </div>
+}
+export default Card;
