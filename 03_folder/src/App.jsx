9ef71e0a67adc1ae11f15f421ccdf17dbfaccc98
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-07 14:45:46 +0530

    Day 3

---
+++ 03_folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      < Card user="Prince" img="https://images.unsplash.com/photo-1761632046628-db5195c5164d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM0OHxDRHd1d1hKQWJFd3x8ZW58MHx8fHx8"/>
+      < Card user="Rajat" img="https://images.unsplash.com/photo-1766179031087-088a7d0e9ae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDgzfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D"/>
+      < Card user="Rishab" img="https://plus.unsplash.com/premium_photo-1763072944317-fc4b2fd81470?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D"/>
+      
+      
+    </div>
+   
+    
+  )
+}
+
+export default App
---
+++ 03_folder/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = (props) => {
+    console.log(props.user);
+  return (
+    
+      <div className="card">
+        <img src={props.img} alt="" />
+        <h1>{props.user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, minus?</p>
+        <button>View Profile</button>
+    </div>
+  )
+}
+
+export default Card
