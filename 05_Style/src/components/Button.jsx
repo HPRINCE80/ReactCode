681e280c60190186d7213c68fcfbb965dca91635
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-13 21:14:34 +0530

    CSS

---
+++ 05_Style/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../Stylecss/Button.css'
+
+const Button = () => {
+  return (
+    <div className='btn'>
+    
+      <button>Sign in</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ 05_Style/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Stylecss/Header.css'
+const Header = () => {
+  return (
+    <div className='head'>
+      Prince Yaduvanshi
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
