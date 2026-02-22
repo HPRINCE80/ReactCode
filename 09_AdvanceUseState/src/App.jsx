c989486c40936da81bb7a3df957b6d386f5e8ebd
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-22 17:19:08 +0530

    State

---
+++ 09_AdvanceUseState/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [num,setnum] = useState(0)
+const btnclicked = () => {
+  setnum(prev => (prev +1))
+  setnum(prev => (prev +1))
+  setnum(prev => (prev +1))
+
+}
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={btnclicked}>Click</button>
+    </div>
+  )
+}
+
+export default App
