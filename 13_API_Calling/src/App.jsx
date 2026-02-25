867acf416f0d7757eeb6df2409f02c29cab0c9e8
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-25 21:54:11 +0530

    API

---
+++ 13_API_Calling/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from axios
+
+const App = () => {
+  const getData = async() => {
+    
+
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
