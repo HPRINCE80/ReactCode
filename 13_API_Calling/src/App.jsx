0842065e0e00c98872a8a74f1c4a3e68a539f032
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-28 13:32:37 +0530

    api calling

--- 13_API_Calling/src/App.jsx
+++ 13_API_Calling/src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import axios from axios
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const App = () => {
-  const getData = async() => {
-    
 
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list")
+    console.log(response.data)
+    setData(response.data)
   }
+
   return (
     <div>
-      
+      <button onClick={getData}>Click</button>
+
+      <div>
+        {data.map(function (elem, idx) {
+          return <h3 >Prince {idx}</h3>
+        })}
+      </div>
+
     </div>
   )
 }
